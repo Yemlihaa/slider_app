0fa2fd2f6748d40e3eedc8f9972d4e3367011280
Yemlihaa <yemlihaalc@gmail.com>
2023-11-27 00:48:19 +0300

    js update

--- script.js
+++ script.js
@@ -13,20 +13,11 @@ var models = [
     name: "Mercedes SLS Amg",
     image: "img/3.jpg",
     link: "https://www.arabam.com/blog/otomobil-inceleme/nissan-skyline-r34-gt-r/",
-  },
-  {
-    name: "Nissan Skyline R34",
-    image: "img/2.jpg",
-    link: "https://www.arabam.com/blog/otomobille-yasam/lamborghini-diablo-hakkinda-bilinmeyenler/",
-  },
-  {
-    name: "Mercedes SLS Amg",
-    image: "img/3.jpg",
-    link: "https://www.arabam.com/blog/otomobil-inceleme/nissan-skyline-r34-gt-r/",
-  },
+  }
 ];
 
-var index = 1;
+var index = 0;
+
 document.querySelector('card-title').textContent = models[index];
 document.querySelector(".card-img-top").setAttribute("src", models[index].image);
 document.querySelector('card-link').setAttribute('herf',models[index].link);
