06177dfde0da22099322d156be561c22569419ae
Yemlihaa <yemlihaalc@gmail.com>
2023-11-28 23:03:35 +0300

    Slider arrow click

--- script.js
+++ script.js
@@ -16,8 +16,48 @@ var models = [
   }
 ];
 
-var index = 2;
 
-document.querySelector('.card-title').textContent = models[index].name;
-document.querySelector('.card-img-top').setAttribute("src", models[index].image);
-document.querySelector('.card-link').setAttribute('herf',models[index].link);
+
+var index = 2;
+var slaytCount = models.length;
+
+showSlide(index);
+
+document.querySelector('.fa-arrow-circle-left').addEventListener('click',function(){
+
+
+  index--;
+  showSlide(index);
+  console.log(index);
+
+
+});
+
+document.querySelector('.fa-arrow-circle-right').addEventListener('click',function(){
+
+
+  index++;
+  showSlide(index);
+  console.log(index);
+
+
+});
+
+function showSlide(i){
+
+  index = i;
+
+  if(i < 0){
+    index = slaytCount - 1;
+  }
+
+  if(i >=  slaytCount){
+    index = 0;
+  }
+
+  document.querySelector('.card-title').textContent = models[index].name;
+  document.querySelector('.card-img-top').setAttribute("src", models[index].image);
+  document.querySelector('.card-link').setAttribute('herf',models[index].link);
+
+}
+
